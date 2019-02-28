a495c82e37964701049a10ab8f8c4c28b6784b44
Joseph Fernandez <joseph.fdez21@gmail.com>
2019-02-28 16:57:35 +0000

    Reorganization

--- toolbox/__init__.py
+++
@@ -1,8 +0,0 @@
-from numpy.random import random, seed
-from scipy.integrate import quad
-
-"""
-from binary import binarySystem as bs
-from binaryGW import binaryGW as bgw
-from petersev import PetersEv as pev
-"""
