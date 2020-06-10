b7f740c1d9c44561baae244087fb6f73622301aa
Joseph Fernandez <joseph.fdez21@gmail.com>
2020-06-10 13:25:07 +0100

    Some changes to see if it is faster

--- mcmcAnalysis.py
+++ mcmcAnalysis.py
@@ -27,7 +27,7 @@ def priors(parameters, ranges):
     return sum(array(vals))
 
 
-def afterglow_likelihood_gaussianJet(model_parameters, data_lc):
+def afterglow_likelihood_gaussianJet(model_parameters):
 
     # model_parameters --> the input to generate the model
     # data_parameters  --> the parameters used to calculate the comparison data (like freqneucies, times, etc)
@@ -47,8 +47,7 @@ def afterglow_likelihood_gaussianJet(model_parameters, data_lc):
 
     jet = Exp.jetHeadGauss(EE, GamC, nn, epsE, epsB, pp, 500, 1e14, 1e22, 'peer', 50, 30.*pi/180, thetaC, aa=1)
     #print jet.TTs.min()
-    #freqs = array(data_lc.keys()).astype('float') # python2
-    freqs = array(list(data_lc)).astype('float')   # python3
+
     tts, lcModel, _, _ = Exp.light_curve_peer_SJ(jet, jet.pp, thetaObs, freqs, 41.3e6*pc, "range", [1.,1000.,100], 1)           # Calculte the model prediction at the observation time
 
     #maskD = [dataType=="lightCurve"]
@@ -58,9 +57,9 @@ def afterglow_likelihood_gaussianJet(model_parameters, data_lc):
     #centShiftFreqs, centShiftdata, centShiftErr = frequencies[maskC] #, data[maskC], data_err[maskC]
 
 
-    for ii, freq in zip(range(len(freqs)), list(data_lc)):
+    for ii, freq in zip(range(len(freqs)), list(lc_dict)):
 
-        times, lc, lc_err = data_lc[freq][0,:], data_lc[freq][1,:], data_lc[freq][2,:]                              # Get the observational times and data
+        times, lc, lc_err = lc_dict[freq][0,:], lc_dict[freq][1,:], lc_dict[freq][2,:]                              # Get the observational times and data
 
         #_, lcModel, _, _ = Exp.light_curve_peer_SJ(jet, jet.pp, thetaObs, freq, 41.3e6*pc, "discrete", times*sTd, 1)           # Calculte the model prediction at the observation time
         lcM = interp1d(tts, lcModel[ii,:])(times*sTd)*1e29
@@ -86,18 +85,18 @@ def afterglow_likelihood_gaussianJet(model_parameters, data_lc):
 
 
 
-def log_probability(parameters, ranges, lc_dict):
+def log_probability(parameters):
     lp = priors(parameters, ranges)
 
     if not isfinite(lp):
         return -1.*inf
 
-    return lp + afterglow_likelihood_gaussianJet(parameters, lc_dict)
+    return lp + afterglow_likelihood_gaussianJet(parameters)
 
 
 
 
-def initialize_walkers(nwalkers, nparameters, ranges):
+def initialize_walkers():
 
     initializations = zeros([nwalkers, nparameters])
 
@@ -120,7 +119,8 @@ freqs = unique(data[:,3])
 nwalkers = 40
 nparameters = len(ranges)
 
-filename = '/data1/arijfern/progress.h5'
+#filename = '/data1/arijfern/progress.h5'
+filename = 'progress.h5'
 backend = emcee.backends.HDFBackend(filename)
 backend.reset(nwalkers, nparameters)
 
@@ -135,14 +135,16 @@ for freq in freqs:
          lc_dict[freq] = array([data[:,1][fil][fil2].astype("float"), data[:,4][fil][fil2].astype("float"), data[:,-1][fil][fil2].astype("float")])
 
 
+#freqs = array(lc_dict.keys()).astype('float') # python2
+freqs = array(list(lc_dict)).astype('float')   # python3
 
-p0 = initialize_walkers(nwalkers, nparameters, ranges)
+p0 = initialize_walkers()
 #sampler = emcee.EnsembleSampler(nwalkers, nparameters, log_probability, args=[ranges, lc_dict], backend=backend, pool=pool)
 
 # Burn-in steps
 with Pool(processes=12) as pool:
     #p0 = initialize_walkers(nwalkers, nparameters, ranges)
-    sampler = emcee.EnsembleSampler(nwalkers, nparameters, log_probability, args=[ranges, lc_dict],backend=backend, pool=pool)
+    sampler = emcee.EnsembleSampler(nwalkers, nparameters, log_probability, backend=backend, pool=pool)
     sampler.run_mcmc(p0, 1000, progress=True)
     sampler.reset()
     print("Finished burner steps")
